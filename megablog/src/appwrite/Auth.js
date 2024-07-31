d29f4ffcedef246e745c2a9cc4fde1c5279ff244
shubhamxcode <shubh.varshneycode@gmail.com>
2024-07-31 11:09:24 +0530

    auth

--- megablog/src/appwrite/Auth.js
+++ megablog/src/appwrite/Auth.js
@@ -11,4 +11,41 @@ export class AuthService{
         .setProject(conf.appwriteprojectid);
         this.account = new Account(this.Client)
     }
+    async createAccount({email,password,name}){
+        try {
+            const userAccount= await this.account.create(ID.unique(), email, password, name)
+            if (userAccount) {
+                // call another method
+                 return this.login({email,password})
+            } else {
+                return userAccount
+            }
+        } catch (error) {
+            throw error
+        }
+    }
+    async login({email,password}){
+        try {
+            return await this. account.createEmailSession(email,password);
+        } catch (error) {
+            throw error
+        }
+    }
+    async getCurrentUser(){
+        try {
+            return await this.account.get();
+        } catch (error) {
+            console.log("Appwrite serive :: getCurrentUser :: error", error);
+        }
+        return null;
+    }
+    async logout(){
+        try {
+            await this.account.deleteSessions()
+        } catch (error) {
+            console.log("Appwrite serive :: getCurrentUser :: error", error);
+        }
+    }
 }
+const authService = new AuthService();
+export default authService
