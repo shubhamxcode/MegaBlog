c8fca05de5ace67ffd2e664af3ee67d7adafd4bd
shubhamxcode <shubh.varshneycode@gmail.com>
2024-07-31 18:17:57 +0530

    database

--- megablog/src/appwrite/config.js
+++ megablog/src/appwrite/config.js
@@ -12,6 +12,55 @@ export class Service{
         this.databases =new Databases(this.Client)
         this.bucket = new Storage(this.Client)
     }
+    async createPost({title,slug,content,featuredImage,status,userId}){
+        try {
+            return await this.databases.createDocument(
+                conf.appwritedatabaseid,
+                conf.appwritecollectionid,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    userId,
+                    status
+                }
+            )
+        } catch (error) {
+            console.log("Appwrite serive :: getCurrentUser :: error", error);
+        }
+    }
+    async updatedocument(slug,{title,content,featuredImage,status,userId}){
+        try {
+           return await this.databases.updateDocument(
+            conf.appwritedatabaseid,
+            conf.appwritecollectionid,
+            slug,
+            {
+                title,
+                content,
+                featuredImage,
+                status,
+            }
+
+           )
+        } catch (error) {
+            console.log("Appwrite serive :: getCurrentUser :: error", error);
+        }
+    }
+    async deletedocument(slug){
+        try {
+            return await this.databases.deleteDocument(
+                conf.appwritedatabaseid,
+                conf.appwritecollectionid,
+                slug
+            )
+            // return true
+        } catch (error) {
+            console.log("Appwrite serive :: getCurrentUser :: error", error);
+            // return false
+        }
+    }
 }
 
 const service= new Service()
