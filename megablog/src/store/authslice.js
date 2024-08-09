a81775516ba703f1394ca4b5d04b65191580ca04
shubhamxcode <shubh.varshneycode@gmail.com>
2024-08-09 09:47:43 +0530

    strore

---
+++ megablog/src/store/authslice.js
@@ -0,0 +1,28 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+
+const initialState={
+    status:true,
+    userData:null
+}
+
+
+
+const authslice=createSlice({
+    name:"auth",
+    initialState,
+    reducers:{
+        login:(state,action)=>{
+            state.status=true,
+            state.userData=action.payload.userData
+        },
+        logout:(state)=>{
+            state.status=false,
+            state.userData=null
+        }
+    }
+})
+
+export const {login,logout}=authslice.actions
+export default authslice.reducer;
